3cc1c74d474324586cb550fe87a44bb0d226c7a0
akshayalakshmivs <akshayalakshmivs16@gmail.com>
2025-02-28 11:18:26 +0530

    Initial commit

---
+++ src/components/salesData.js
@@ -0,0 +1,9 @@
+export const salesData = [
+    { month: "Jan", sales: 4000 },
+    { month: "Feb", sales: 3000 },
+    { month: "Mar", sales: 5000 },
+    { month: "Apr", sales: 7000 },
+    { month: "May", sales: 6000 },
+    { month: "Jun", sales: 8000 },
+  ];
+  
